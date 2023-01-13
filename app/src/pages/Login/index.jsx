2cc2f752d35caba6dc06973567a14933a216df25
yira <dladlfk1@gmail.com>
2023-01-13 16:45:19 +0900

    feat:login-style-prototype

--- app/src/pages/Login/index.jsx
+++ app/src/pages/Login/index.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import Form from './Form';
+import styles from './style.module.css';
+import Social from './Social';
 
 const Login = () => {
   return (
-    <div>
-      <div>
-        <h1>로그인</h1>
-        <image></image>
+    <div className={styles.container}>
+      <div className={styles.loginContainer}>
+        <div className={styles.navBox}>
+          <h1 className={styles.loginH1}>로그인</h1>
+          <div className={styles.imageBox}></div>
+        </div>
+        <span className={styles.span}>오늘부터 당신의 생각을 세상과 공유하세요</span>
+        <section className={styles.sectionForm}>
+          <Form />
+        </section>
+        <section className={styles.sectionSocial}>
+          <Social></Social>
+        </section>
       </div>
-      <span>오늘부터 당신의 생각을 세상과 공유하세요</span>
-      <section>
-        <Form />
-      </section>
     </div>
   );
 };
