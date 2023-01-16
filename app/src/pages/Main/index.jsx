d1c5e52cc8d37a5347b1543a67c71f0fbb16cb40
hoseol OH <dhgh9590@naver.com>
2023-01-16 16:19:15 +0900

    post 페이지  완성

--- app/src/pages/Main/index.jsx
+++ app/src/pages/Main/index.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import styles from './style.module.css';
 import Nav from '../../components/Nav';
 import Location from './Location';
+import Post from './Post';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 export const Main = () => {
   return (
@@ -10,6 +13,34 @@ export const Main = () => {
       <section className={styles.section}>
         <div className={styles.container}>
           <Location></Location>
+          <div className={styles.top_nav}>
+            <div className={styles.category}>
+              <ul>
+                <li>
+                  <em>전체</em>
+                </li>
+                <li>
+                  <em>동네소식</em>
+                </li>
+                <li>
+                  <em>동네질문</em>
+                </li>
+                <li>
+                  <em>일상</em>
+                </li>
+                <li>
+                  <em>분실/실종</em>
+                </li>
+              </ul>
+            </div>
+            <div className={styles.writing}>
+              <button className={styles.btn}>
+                <FontAwesomeIcon icon={faPlus} className={styles.icon} />
+                <em>글쓰기</em>
+              </button>
+            </div>
+          </div>
+          <Post></Post>
         </div>
       </section>
     </>
