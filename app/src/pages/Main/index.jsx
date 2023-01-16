36dade42bdc2e1d4c893579d9007389d01185611
hoseol OH <dhgh9590@naver.com>
2023-01-16 13:41:57 +0900

    address 버튼

--- app/src/pages/Main/index.jsx
+++ app/src/pages/Main/index.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import styles from './style.module.css';
 import Nav from '../../components/Nav';
+import Location from './Location';
 
 export const Main = () => {
   return (
     <>
       <Nav></Nav>
-      <section className={styles.section}>a</section>
+      <section className={styles.section}>
+        <div className={styles.container}>
+          <Location></Location>
+        </div>
+      </section>
     </>
   );
 };
