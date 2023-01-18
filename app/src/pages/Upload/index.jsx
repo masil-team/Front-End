ecdbb001b62a85dec521f64cb8e4fd27a9ca8cea
yira <dladlfk1@gmail.com>
2023-01-18 19:54:25 +0900

    feat:25-createPost(#25)

--- app/src/pages/Upload/index.jsx
+++ app/src/pages/Upload/index.jsx
@@ -4,6 +4,13 @@ import styles from './style.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCamera } from '@fortawesome/free-solid-svg-icons';
 import { useForm } from 'react-hook-form';
+
+// const hidden = {
+//   initial: {},
+//   animate: {},
+//   exit: {},
+// };
+
 const CreatePost = () => {
   const { register } = useForm();
   const arr = Array.from({ length: 10 }, (key, index) => index + 1);
@@ -28,7 +35,17 @@ const CreatePost = () => {
           <div className={styles.secondsection}>
             <textarea {...register('text')} type="text" placeholder="당신의 이야기를 적어주세요"></textarea>
           </div>
-          <div className={styles.thirdsection}></div>
+          <div className={styles.thirdsection}>
+            <div className={styles.gridBox}>
+              {arr.map(i => {
+                return (
+                  <div className={styles.gridItem} key={i}>
+                    {i}
+                  </div>
+                );
+              })}
+            </div>
+          </div>
         </div>
       </div>
     </>
