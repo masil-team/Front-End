acf61bb645aa304f2cd27c4e325bee2e7473d84d
yira <dladlfk1@gmail.com>
2023-02-07 10:00:32 +0900

    chore:categoryBox-style-chage

--- app/src/pages/Upload/Form/index.jsx
+++ app/src/pages/Upload/Form/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCamera } from '@fortawesome/free-solid-svg-icons';
+import { faCamera, faSortDown, faSortUp } from '@fortawesome/free-solid-svg-icons';
 import styles from './style.module.css';
 import { useForm } from 'react-hook-form';
 import PropTypes from 'prop-types';
@@ -8,25 +8,43 @@ import axios from '../../../utils/token';
 import { useNavigate } from 'react-router-dom';
 import { AnimatePresence, motion } from 'framer-motion';
 import { useState } from 'react';
-
-const sliding = {
+const subMenuAnimate = {
   initial: {
-    y: '-100%',
     opacity: 0,
+    rotateX: -15,
+    transition: {
+      duration: 0.2,
+      delay: 0.1,
+    },
+    transitionEnd: {
+      display: 'none',
+    },
   },
   animate: {
-    y: 0,
     opacity: 1,
+    rotateX: 0,
+    transition: {
+      duration: 0.2,
+    },
+    display: 'block',
   },
   exit: {
-    y: '-100%',
     opacity: 0,
+    rotateX: -15,
+    transition: {
+      duration: 0.2,
+      delay: 0.1,
+    },
+    transitionEnd: {
+      display: 'none',
+    },
   },
 };
 
 const Form = ({ image, setImage, count, setCount }) => {
   const { register, getValues } = useForm();
   const [show, setShow] = useState(false);
+  const [category, setCategory] = useState({ text: '카테고리', val: 0 });
   const nav = useNavigate();
   const categories = [
     { text: '동네소식', val: 1 },
@@ -46,32 +64,43 @@ const Form = ({ image, setImage, count, setCount }) => {
   };
 
   const uploadData = async () => {
-    const category = getValues('category');
-    const text = getValues('text');
-    try {
-      const response = await axios.post('http://13.209.94.72:8080/posts', { content: text, boardId: category });
-      if (response.status === 201) {
-        alert('게시물 업로드 완료');
-        sessionStorage.removeItem('postList');
-        sessionStorage.removeItem('pageNum');
-        nav('/');
+    const boardId = category.val;
+    if (boardId === 0) {
+      alert('카테고리를 선택해주세요.');
+    } else {
+      const text = getValues('text');
+      try {
+        const response = await axios.post('http://13.209.94.72:8080/posts', { content: text, boardId });
+        if (response.status === 201) {
+          alert('게시물 업로드 완료');
+          sessionStorage.removeItem('postList');
+          sessionStorage.removeItem('pageNum');
+          nav('/');
+        }
+      } catch (err) {
+        console.log(err);
       }
-    } catch (err) {
-      console.log(err);
+      console.log(boardId, text, image);
     }
-    console.log(category, text, image);
   };
 
-  console.log(show);
   return (
     <>
       <div className={styles.firstsection}>
         <div className={styles.categoryBox}>
           <ul onClick={() => setShow(prev => !prev)} className={styles.ul}>
+            <span>{category.text}</span>
+            <span>
+              {show ? (
+                <FontAwesomeIcon className={styles.icon1} icon={faSortUp} />
+              ) : (
+                <FontAwesomeIcon className={styles.icon2} icon={faSortDown} />
+              )}
+            </span>
             <AnimatePresence>
               {show && (
                 <motion.div
-                  variants={sliding}
+                  variants={subMenuAnimate}
                   transition={{ duration: 0.3 }}
                   initial="initial"
                   animate="animate"
@@ -80,7 +109,7 @@ const Form = ({ image, setImage, count, setCount }) => {
                   {categories.map(i => (
                     <>
                       <li key={i.val} className={styles.li}>
-                        <button>{i.text}</button>
+                        <button onClick={() => setCategory(i)}>{i.text}</button>
                       </li>
                     </>
                   ))}
@@ -88,10 +117,12 @@ const Form = ({ image, setImage, count, setCount }) => {
               )}
             </AnimatePresence>
           </ul>
+
           <button onClick={uploadData} className={styles.btn}>
             올리기
           </button>
         </div>
+
         <div onClick={getImages} className={styles.photoupload}>
           <FontAwesomeIcon style={{ fontSize: '32px' }} icon={faCamera} />
           <span style={{ fontSize: '16px' }}>사진올리기</span>
