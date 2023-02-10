33c61b5cec286cf42dde11c8468a389ef2672a29
hoseol OH <dhgh9590@naver.com>
2023-02-10 16:06:50 +0900

    임시 수정

--- app/src/index.js
+++ app/src/index.js
@@ -12,7 +12,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <CookiesProvider>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
       <Provider store={store}>
         <ScrollToTop />
         <App />
